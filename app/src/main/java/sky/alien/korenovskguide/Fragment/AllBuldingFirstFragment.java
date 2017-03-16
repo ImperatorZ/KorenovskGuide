731d634884e17c2e631732f9ba5f317f552f3d8d
ImperatorZ <asassins7770@gmail.com>
2017-03-16 14:44:34 +0300

    First

---
+++ app/src/main/java/sky/alien/korenovskguide/Fragment/AllBuldingFirstFragment.java
@@ -0,0 +1,32 @@
+package sky.alien.korenovskguide.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import sky.alien.korenovskguide.R;
+
+/**
+ * Created by NeoGhost on 16.03.2017.
+ */
+
+public class AllBuldingFirstFragment extends Fragment {
+
+    public AllBuldingFirstFragment(){
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.bulding_layout, container, false);
+    }
+}
